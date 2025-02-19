f9ef08bf0fc3c964302f572c19eb747c07c02002
xyt <xyt@DESKTOP-KLNFC90.dhcp.wustl.edu>
2025-02-19 15:49:42 -0600

    Yeah!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,51 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+	    int redComponent = 110;
+	    int greenComponent = 111;
+	    int blueComponent = 150;
+
+	    
+	    double parameterOne = 0.5;
+	    double parameterTwo = 0.5;
+	    double parameterThree = 0.3;
+	    double parameterFour = 0.2;
+	    
+	    StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+	    StdDraw.rectangle(parameterOne,parameterTwo, parameterThree, parameterFour);
+	 
+	    
+	    int redComponent2 = 180;
+	    int greenComponent2 = 40;
+	    int blueComponent2 = 220;
+	    
+	    double parameterOne2 = 0.5;
+	    double parameterTwo2 = 0.5;
+	    double parameterThree2 = 0.3;
+	    double parameterFour2 = 0.2;
+	    
+	    StdDraw.setPenColor(redComponent2,greenComponent2,blueComponent2);
+	    StdDraw.filledEllipse(parameterOne2, parameterTwo2, parameterThree2, parameterFour2);
+	    
+	    int redComponent3 = 100;
+	    int greenComponent3 = 44;
+	    int blueComponent3 = 99;
+	    
+	    double parameterOne3 = 0.5;
+	    double parameterTwo3 = 0.3;
+	    double parameterThree3 = 0.7;
+	    double parameterFour3= 0.7;
+	    double parameterFive = 0.3;
+    	double parameterSix = 0.3;
+    	
+    	
+    	double [] X= {parameterOne3, parameterTwo3, parameterThree3};
+		double [] Y= {parameterFour3, parameterFive, parameterSix};
+	    
+	    StdDraw.setPenColor(redComponent3,greenComponent3,blueComponent3);
+	    StdDraw.filledPolygon(X,Y);
+	 
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,46 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		//imput
+		String shapeType = in.next();
+	    int redComponent = in.nextInt();
+	    int greenComponent = in.nextInt();
+	    int blueComponent = in.nextInt();
+	    boolean isFilled = in.nextBoolean();
+	    
+	    double parameterOne = in.nextDouble();
+	    double parameterTwo = in.nextDouble();
+	    double parameterThree = in.nextDouble();
+	    double parameterFour = in.nextDouble();
+	    
+	    StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+	    
+	    if(shapeType.equals("rectangle")) {
+	    	if(isFilled== true) {
+	    	    StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+	    	    }else {
+	    	    	StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+	    	    }
+	    }else if(shapeType.equals("ellipse")){
+	    	if(isFilled== true) {
+	    	    StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+	    	    }else {
+	    	    	StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+	    	    }
+	    	
+	    }else if(shapeType.equals("triangle")){
+	    	double parameterFive = in.nextDouble();
+	    	double parameterSix = in.nextDouble();
+	    	double [] X= {parameterOne, parameterTwo, parameterThree};
+    		double [] Y= {parameterFour, parameterFive, parameterSix};
+    		
+	    	if(isFilled== true) {
+	    		 StdDraw.filledPolygon(X,Y);
+	    	    }else {
+	    	    	StdDraw.polygon(X,Y);
+	    	    	
+	    	    }
+	    }
+	    
 	}
 }
